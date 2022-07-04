97edc77be1e4a810322bd62de4cd105b9e249588
anamar3 <105776959+anamar3@users.noreply.github.com>
2022-07-04 12:09:41 +0300

    Add files via upload

---
+++ LabGenerics/3. Generic Scale/EqualityScale.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericScale
+{
+    class EqualityScale<T>
+    {
+         T left;
+        T right;
+        public EqualityScale(T left, T right)
+        {
+
+        }
+       public bool AreEqual()
+        {
+            if (this.right.Equals(this.left))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ LabGenerics/LabGenerics/Box.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class Box<T>
+{
+        private List<T> data;
+        public Box()
+        {
+             Data = new List<T>();
+        }
+
+        public int Count =>  Data.Count;
+        public List<T> Data { get; set; }
+
+        public void Add(T element)
+        {
+            Data.Add(element);
+        }
+        public  T Remove()
+        {
+            var rem = Data.Last();
+            Data.RemoveAt(Data.Count - 1);
+            return rem;
+            
+        }
+
+}
+}
